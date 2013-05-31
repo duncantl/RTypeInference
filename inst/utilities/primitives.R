c59ec815b33acfe68ada9be3ebc624b4bed8baf4
Duncan Temple Lang <duncan@r-project.org>
2013-05-31 12:45:37 -0700

    code and results for reading the names of the primitives from names.c

---
+++ inst/utilities/primitives.R
@@ -0,0 +1,33 @@
+
+library(RCIndex)
+
+f = sprintf("%s/../src/main/names.c", R.home())
+if(!file.exists(f))
+  stop("cannot find names.c")
+
+tu = createTU(f, args = "-DHAVE_CONFIG_H", includes = c(sprintf("%s/../src/include", R.home()), sprintf("%s/include", R.home()), sprintf("%s/src/include", R.home())))
+
+gvars = getGlobalVariables(tu)
+
+gvars$R_FunTab
+gvars$R_FunTab@def$kind # VarDecl
+
+gvars$R_FunTab@def[[2]]$kind  # InitListExpr
+
+length(gvars$R_FunTab@def[[2]]) # 682
+
+init = gvars$R_FunTab@def[[2]]
+k = sapply(children(init), function(x) x$kind)
+table(k)
+
+primitiveNames = structure(sapply(children(init), function(x) getCursorTokens(x[[1]])[1]), names = NULL)
+
+primitiveNames = primitiveNames[!is.na(primitiveNames)]
+primitiveNames = gsub('"', '', primitiveNames)
+
+
+con = file("../../R/primitiveNames.R", "w")
+cat("PrimitiveNames = ", file = con)
+dput(primitiveNames, con)
+close(con)
+
