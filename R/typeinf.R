c5c88722d30a41c8d9c5066cbef72a6361589851
Duncan Temple Lang <duncan@r-project.org>
2014-07-03 15:58:53 -0700

    this should not be in the R directory of the package

--- R/typeinf.R
+++ R/typeinf.R
@@ -1,6 +1,6 @@
-source("known_types.R")
-source("test_functions.R")
-source("hm.R")
+#source("known_types.R")
+#source("test_functions.R")
+#source("hm.R")
 
-test = inferType(test_function)
-print(test)
+#test = inferType(test_function)
+#print(test)
