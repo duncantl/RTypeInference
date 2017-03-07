cd1ae7b829feee32a737cd5781399a23720ff3e4
Nick Ulle <nick.ulle@gmail.com>
2017-03-06 18:34:39 -0800

    Use default arguments when generating constraints.

--- R/constrain.R
+++ R/constrain.R
@@ -6,18 +6,23 @@
 #'
 #' @export
 constrain = function(cfg
-  , id = 1L
+  , b = 1L
   , dom_t = ast::dom_tree(cfg)
   , set = ConstraintSet$new()
 ) {
+  block = cfg[[b]]
+
+  if (is(block, "FnEntryBlock"))
+    lapply(block$params, constrain_ast, set)
+
   # Iterate over Phi nodes.
-  lapply(cfg[[id]]$phi, constrain_ast, set)
+  lapply(block$phi, constrain_ast, set)
 
   # Iterate over body, generating type constraints.
-  lapply(cfg[[id]]$body, constrain_ast, set)
+  lapply(block$body, constrain_ast, set)
 
   # Descend to next blocks.
-  children = setdiff(which(dom_t == id), id)
+  children = setdiff(which(dom_t == b), b)
   lapply(children, function(i) constrain(cfg, i, dom_t, set))
 
   return (set)
@@ -36,6 +41,18 @@ constrain_ast.Assign = function(node, set) {
   set$append(node$write$name, type)
 
   return (type)
+}
+
+#' @export
+constrain_ast.Parameter = function(node, set) {
+  if (is.null(node$default))
+    return (NULL)
+
+  type = constrain_ast(node$default, set)
+
+  set$append(node$name, type)
+
+  return(type)
 }
 
 #' @export
---
+++ tests/testthat/test_constrain.R
@@ -0,0 +1,28 @@
+context("constrain")
+
+
+test_that("constraints generated from default arguments", {
+  code = function(n = 0L) {
+    n = n - 1
+    return (n)
+  }
+
+  cfg = ast::to_cfg(ast::to_ast(code))
+  result = constrain(cfg)
+
+  # -----
+  expect_is(result[[1]][[2]], "typesys::IntegerType")
+})
+
+
+test_that("no error when default arguments are unspecified", {
+  code = function(x) {
+    return (x)
+  }
+
+  cfg = ast::to_cfg(ast::to_ast(code))
+  result = constrain(cfg)
+
+  # -----
+  # Okay as long as there was no error.
+})
