duncantl/RTypeInference

commit 7be09149b1cde7a758ea8363e9dba5bb89e1ec75

Fix constraint bug due to rstatic update.

R/constrain.R
@@ -6,7 +6,7 @@
 #'
 #' @export
 constrain = function(cfg
-  , b = cfg$entry
+  , b = cfg$get_index(cfg$entry)
   , dom_t = rstatic::dom_tree(cfg)
   , set = ConstraintSet$new()
 ) {

commit b906d2144d499c5de12e03c694d6ea9cd246174b

Fix for bug caused by 7be0914 and fbc0e05.

R/constrain.R
@@ -6,13 +6,18 @@
 #'
 #' @export
 constrain = function(cfg
-  , b = cfg$get_index(cfg$entry)
+  , b
   , dom_t = rstatic::dom_tree(cfg)
   , set = ConstraintSet$new()
 ) {
-  # TODO: Generate constraints for parameters in caller, before recursion.
-  if (b == cfg$entry && !is.null(cfg$params))
-    lapply(cfg$params, constrain_ast, set)
+
+  if (missing(b)) {
+    b = cfg$get_index(cfg$entry)
+
+    # TODO: Generate constraints for parameters in caller, before recursion.
+    if (!is.null(cfg$params))
+      lapply(cfg$params, constrain_ast, set)
+  }
 
   block = cfg[[b]]
 