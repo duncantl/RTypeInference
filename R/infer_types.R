b9a1910d5e3a29b02ef64e0022093e0e50fd790c
Duncan Temple Lang <duncan@r-project.org>
2017-06-23 07:44:49 -0700

    if the user doesn't give names for the init list, use the names of the parameters. Need to do a little more to match the call.  Added methods for infer_types for a call and a function

--- R/infer_types.R
+++ R/infer_types.R
@@ -9,31 +9,6 @@ infer_types = function(code, init = list(), scalar = FALSE, ...) {
   UseMethod("infer_types")
 }
 
-infer_types.function =
-function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
-          ConstrainHandlers = getConstrainHandlers(),  ...)
-{
-   infer_types(rstatic::to_cfg(code), init, scalar, set, ConstrainHandlers, ...)
-}
-
-infer_types.call =
-function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
-          ConstrainHandlers = getConstrainHandlers(),  ...)
-{
-   f = function() x
-   body(f) = code
-   infer_types(f, init, scalar, set, ConstrainHandlers, ...)
-}
-
-`infer_types.{` =
-function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
-          ConstrainHandlers = getConstrainHandlers(),  ...)
-{
-   f = function() {}
-   body(f) = code
-   infer_types(f, init, scalar, set, ConstrainHandlers, ...)
-}
-
 
 #' @export
 infer_types.ControlFlowGraph =
@@ -42,6 +17,9 @@ function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
 {
   if(length(init) && !is(init, "ConstraintSet")) {
        #XXX This should be in the initialization method for set.
+     if(length(names(init)) == 0) 
+         names(init) = names(cfg$params)[seq(along = init)]
+
      mapply(function(type, name)
              set$append(name, type),  # Need to convert the types from user-convenient types to those we expect (in typesys?)
             init, paste0(names(init), "_1"))
@@ -58,5 +36,38 @@ function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
 infer_types.default = function(code, init = list(), ...) {
   cfg = rstatic::to_cfg(rstatic::to_ast(code), in_place = TRUE)
 
-  infer_types(cfg)
+  infer_types(cfg, init, ...)
+}
+
+
+# The default method may handle the following. If so, remove these.
+# However the infer_types.call should probably grab the values of the arguments.
+
+infer_types.function =
+function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
+          ConstrainHandlers = getConstrainHandlers(),  ...)
+{
+   infer_types(rstatic::to_cfg(code), init, scalar, set, ConstrainHandlers, ...)
+}
+
+# This is probably not useful for calls such as x + 1 where we don't know x
+# but we a) we can get the current type of x from the workspace, and
+# b) calls such as foo(2) do make sense - we can get the return type.
+infer_types.call =
+function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
+          ConstrainHandlers = getConstrainHandlers(),  ...)
+{
+   f = function() x
+   body(f) = code
+   infer_types(f, init, scalar, set, ConstrainHandlers, ...)
+   # probably want the return type.
+}
+
+`infer_types.{` =
+function(code, init = list(), scalar = FALSE, set = ConstraintSet$new(),
+          ConstrainHandlers = getConstrainHandlers(),  ...)
+{
+   f = function() {}
+   body(f) = code
+   infer_types(f, init, scalar, set, ConstrainHandlers, ...)
 }
