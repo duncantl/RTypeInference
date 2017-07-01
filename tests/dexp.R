e08574c30eccf4b3e1ac0a45518ee4e9d08e1a8d
Duncan Temple Lang <duncan@r-project.org>
2017-07-01 14:29:28 -0700

    ...

--- tests/dexp.R
+++ tests/dexp.R
@@ -4,12 +4,16 @@ library(RTypeInference)
 rdexp =
 function(x, lambda = 1.0)
 {
-  lambda * exp( - x * lambda)
+    #FIXME - get the one w/o the () after the -
+#  return(lambda * exp( - x * lambda))
+  y = lambda * exp( - (x * lambda))
+  return(y)
 }
 
 # Not clear we can tell if this a scalar or a vector of RealType.
 # Can we make RTypeInference do this based on its knowledge of exp()
 
 cfg = to_cfg(rdexp)
-types = infer_types(cfg)
+types = infer_types(cfg, scalar = TRUE)
+
 
